20eb456fd4f337f7628f90f0894bfa1b09106524
Siddhant <sidpawbake@gmail.com>
2025-03-15 14:59:01 +0530

    DB connected!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import {createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABykRd2fghI-8Z05r54tX6Ian09Sd1-AA",
@@ -11,35 +12,24 @@ const firebaseConfig = {
   appId: "1:390686401782:web:cd9e614b77a9cc21f578d7",
   measurementId: "G-RCXREFWNLC"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth=getAuth();
 const analytics = getAnalytics(app);
-
-const fullName = document.getElementById('fullName');
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-const confirmPassword = document.getElementById('confirmPassword').value;
 const submit = document.getElementById('submit');
-  submit.addEventListener('click',function(event) {
+submit.addEventListener('click',function(event) {
     event.preventDefault();
-    if (fullName.value === '' || email.value === '' || password.value === '' || confirmPassword.value === '') {
-      alert('All fields are required');
-    } else if (!isValidEmail(email)) {
-      alert('Please enter a valid email address');
-    } else if (password.length < 8 || password.search(/[a-z]/) === -1 || password.search(/[A-Z]/) === -1 || password.search(/[0-9]/) === -1 || password.search(/[!@#$%^&*()]/) === -1) {
-      alert('Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character');
-    } else if (password!== confirmPassword) {
-      alert('Passwords do not match');
-    }
-    createUserWithEmailandPassword(auth,email,password)
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth,email,password)
     .then((userCredential)=>{
       const user=userCredential.user;
       alert("Successfully created")
     })
      .catch((Error) => {
-      const errorCode=error.code;
-      const errorMessage=error.message;
+      const errorCode=Error.code;
+      const errorMessage=Error.message;
       alert(errorCode,errorMessage);
      });
     });
